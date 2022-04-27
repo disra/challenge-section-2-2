7c9160307caa5212e7aef46c0b7df559d85dcbea
Disra Udomboonyaluck <disra.udomboonyaluck@seagate.com>
2022-04-27 16:21:58 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Container, Row, Col, Form } from "react-bootstrap";
 
 function App() {
+  const [categories, setCategories] = useState([]);
+  const [result, setResult] = useState([]);
+
+  const getData = () => {
+    fetch("https://api.publicapis.org/categories")
+      .then((res) => res.json())
+      .then((data) => {
+        setResult(data.categories);
+        setCategories(data.categories);
+      });
+  };
+
+  const handleFilter = (text) => {
+    setResult(
+      categories.filter((item) =>
+        item.toLowerCase().includes(text.toLowerCase())
+      )
+    );
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Form.Control
+          className="my-3"
+            type="text"
+            onChange={(e) => handleFilter(e.target.value)}
+          ></Form.Control>
+          <div className="d-flex flex-wrap">
+            {result.map((item) => {
+              return <div className="h5 mx-3 my-1">{item}</div>;
+            })}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
